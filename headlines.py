1672457c720978d4d88b1b9dc18e4119bf0c70b3
664956016 <13060820957tzh@gmail.com>
2017-12-18 21:32:56 +0800

    change some files

--- headlines.py
+++ headlines.py
@@ -3,6 +3,7 @@
 from flask import Flask 
 import feedparser
 from flask import render_template
+from flask import request
 
 app = Flask(__name__)
 
@@ -13,13 +14,15 @@ RSS_FEEDS = {
 'iol':'http://www.iol.co.za/cmlink/1.640'
 }
 
-@app.route("/")
-@app.route("/<publication>")
-    
-def get_news(publication='bbc'):
-    feed = feedparser.parse(RSS_FEEDS[publication])
-    first_article = feed['entries'][0]
-    return render_template("home.html", articles=feed['entries'])
+@app.route("/")  
+def get_news():
+	query = request.args.get('publication')
+	if not query or query.lower() not in RSS_FEEDS:
+		publication = 'bbc'
+	else:
+		publication = query.lower()
+	feed = feedparser.parse(RSS_FEEDS[publication])
+	return render_template("home.html", articles=feed['entries'])
 
 if __name__ == "__main__":
     app.run(port=5000, debug=True)
